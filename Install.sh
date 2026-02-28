9e60686d4c567fa2dbe47e260e662e4f2fcc38eb
race-in <shyamalmurmu740@gmail.com>
2026-03-01 01:03:57 +0530

    Change seedbox installation script source URL
    Updated the source URL for seedbox installation script.

--- Install.sh
+++ Install.sh
@@ -377,7 +377,7 @@ touch /root/.boot-script.sh && chmod +x /root/.boot-script.sh
 cat << EOF > /root/.boot-script.sh
 #!/bin/bash
 sleep 120s
-source <(wget -qO- https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/seedbox_installation.sh)
+source <(wget -qO- https://raw.githubusercontent.com/race-in/dedi-components/main/seedbox_installation.sh)
 # Check if Seedbox Components is successfully loaded
 if [ \$? -ne 0 ]; then
 	exit 1
